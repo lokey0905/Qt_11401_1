359f652de5112fdcb08b79021c8e2180af8ac7bf
hi-ma-san <41143161@nfu.edu.tw>
2026-01-04 00:12:41 +0800

    feat: finalize settings tab with tray integration and auto-start

--- Core/SettingsManager.cpp
+++ Core/SettingsManager.cpp
@@ -1,11 +1,14 @@
 #include "SettingsManager.h"
 #include <QDebug>
+#include <QStandardPaths>
+#include <QDir>
+#include <QFile>
+#include <QCoreApplication>
 
 SettingsManager* SettingsManager::m_instance = nullptr;
 QMutex SettingsManager::m_mutex;
 
 SettingsManager::SettingsManager(QObject *parent) : QObject(parent) {
-    // 初始化 QSettings，將設定存在程式執行目錄下的 config.ini
     m_settings = new QSettings("config.ini", QSettings::IniFormat, this);
 
     // 啟動時自動載入之前存過的數值
@@ -23,13 +26,41 @@ SettingsManager* SettingsManager::instance() {
     return m_instance;
 }
 
+/** --- 開機啟動邏輯 捷徑--- **/
+
+bool SettingsManager::isAutoStart() const {
+    // 取得 Windows 使用者啟動資料夾下的 .lnk 路徑
+    QString startupPath = QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation)
+                          + "/Startup/Qt_11401_1.lnk";
+    return QFile::exists(startupPath);
+}
+
+void SettingsManager::setAutoStart(bool enable) {
+    QString startupDir = QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation) + "/Startup/";
+    QString shortcutPath = startupDir + "Qt_11401_1.lnk";
+    QString appPath = QCoreApplication::applicationFilePath();
+
+    if (enable) {
+        if (!QFile::exists(shortcutPath)) {
+            // 建立指向 Qt_11401_1.exe 的捷徑
+            QFile::link(appPath, shortcutPath);
+            qDebug() << "SettingsManager: 已建立啟動捷徑" << shortcutPath;
+        }
+    } else {
+        if (QFile::exists(shortcutPath)) {
+            QFile::remove(shortcutPath);
+            qDebug() << "SettingsManager: 已移除啟動捷徑";
+        }
+    }
+}
+
+/** --- 全域設定套用 --- **/
+
 void SettingsManager::setGlobalDragLocked(bool locked) {
     if (m_globalDragLocked != locked) {
         m_globalDragLocked = locked;
-        // 同步寫入檔案
         m_settings->setValue("Global/DragLocked", locked);
         emit globalDragLockedChanged(locked);
-        qDebug() << "SettingsManager: 全域拖曳鎖定已儲存 ->" << locked;
     }
 }
 
@@ -37,7 +68,10 @@ void SettingsManager::setShowTrayIcon(bool show) {
     if (m_showTrayIcon != show) {
         m_showTrayIcon = show;
         m_settings->setValue("Global/ShowTrayIcon", show);
+
+        // 發出訊號通知主視窗
         emit trayIconSettingChanged(show);
-        qDebug() << "SettingsManager: 托盤顯示設定已儲存 ->" << show;
+
+        qDebug() << "SettingsManager: 托盤圖示狀態更新為 ->" << show;
     }
 }
